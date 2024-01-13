5b7423fa2ee487a0ee27cc92389ff046e9f1faa9
gsjoy24 <gour.joy24@gmail.com>
2024-01-13 23:51:04 +0600

    working

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { Button } from '../ui/button';
 import {
 	Dialog,
@@ -12,34 +13,48 @@ import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
 const AddTodoModal = () => {
+	const [task, setTask] = useState('');
+	const [description, setDescription] = useState('');
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(task, description);
+	};
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
 				<Button className="bg-gradient-to-tr  from-gray-800 to-gray-600 text-white rounded-[4px]">Add Todo</Button>
 			</DialogTrigger>
-			<DialogContent className="sm:max-w-[425px] bg-white rounded-[8px]">
-				<DialogHeader>
-					<DialogTitle>Add Todo</DialogTitle>
-					<DialogDescription>Add a new todo to your list.</DialogDescription>
-				</DialogHeader>
-				<div className="grid gap-4 py-4">
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="task" className="text-right">
-							Task
-						</Label>
-						<Input id="task" placeholder="New task" className="col-span-3" />
+			<form onSubmit={onSubmit}>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>Add Todo</DialogTitle>
+						<DialogDescription>Add a new todo to your list.</DialogDescription>
+					</DialogHeader>
+					<div onSubmit={onSubmit} className="grid gap-4 py-4">
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="task" className="text-right">
+								Task
+							</Label>
+							<Input onBlur={(e) => setTask(e.target.value)} id="task" placeholder="New task" className="col-span-3" />
+						</div>
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="description" className="text-right">
+								Description
+							</Label>
+							<Input
+								onBlur={(e) => setDescription(e.target.value)}
+								id="description"
+								placeholder="Description"
+								className="col-span-3"
+							/>
+						</div>
 					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="description" className="text-right">
-							Description
-						</Label>
-						<Input id="description" placeholder="Description" className="col-span-3" />
+					<div>
+						<Button type="submit">Save changes</Button>
 					</div>
-				</div>
-				<DialogFooter>
-					<Button type="submit">Save changes</Button>
-				</DialogFooter>
-			</DialogContent>
+				</DialogContent>
+			</form>
 		</Dialog>
 	);
 };
